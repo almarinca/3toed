920e1b9ec726c10c7570fc38a05e8053c8531f44
Alejandro Marín Cardona <almarinca@unal.edu.co>
2025-01-22 09:43:15 -0500

    Initial product model

--- ttoed/products/models.py
+++ ttoed/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name="Product Name")
+    description = models.TextField(max_length=300, verbose_name="Product Description")
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Product Price"
+    )
+    available = models.BooleanField(default=True, verbose_name="Product Available")
+    image = models.ImageField(
+        upload_to="prod_imgs", null=True, blank=True, verbose_name="Product image"
+    )
+
+    def __str__(self):
+        return self.name
