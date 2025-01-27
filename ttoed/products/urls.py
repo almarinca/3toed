cc4b193122453087599d7ba41371662eb27dc180
Alejandro Marín Cardona <almarinca@unal.edu.co>
2025-01-27 12:54:10 -0500

    Create view for the product list

--- ttoed/products/urls.py
+++ ttoed/products/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import ProductFormView
+from .views import ProductFormView, ProductListView
 
-urlpatterns = [path("add/", ProductFormView.as_view(), name="add_product")]
+urlpatterns = [
+    path("add/", ProductFormView.as_view(), name="add_product"),
+    path("", ProductListView.as_view(), name="product_list")
+]
--- ttoed/products/views.py
+++ ttoed/products/views.py
@@ -3,12 +3,17 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import productForm
+from .models import Product
 
 class ProductFormView(generic.FormView):
     template_name = "products/add_product.html"
     form_class = productForm
-    success_url = reverse_lazy("add_product")
+    # success_url = reverse_lazy("add_product")
+    success_url = reverse_lazy("product_list")
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+class ProductListView(generic.ListView):
+    model = Product
