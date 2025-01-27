cfc879728b11661c15e1eccfcb612187d95d2616
Alejandro Marín Cardona <almarinca@unal.edu.co>
2025-01-27 12:55:23 -0500

    set up django routing to media dirs, show imgs

--- ttoed/products/models.py
+++ ttoed/products/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     )
     available = models.BooleanField(default=True, verbose_name="Product Available")
     image = models.ImageField(
-        upload_to="prod_imgs", null=True, blank=True, verbose_name="Product image"
+        upload_to="media", null=True, blank=True, verbose_name="Product image"
     )
 
     def __str__(self):
