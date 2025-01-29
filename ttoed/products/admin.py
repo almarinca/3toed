f500df186ae92209d7f9d645cc805975495ba4cb
Alejandro Marín Cardona <almarinca@unal.edu.co>
2025-01-29 09:41:22 -0500

    Register Product model in Django admin

--- ttoed/products/admin.py
+++ ttoed/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+admin.site.register(Product, ProductAdmin)
